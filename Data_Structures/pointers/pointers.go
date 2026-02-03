723a0a928e72b51fc129dc02bd9c8949bfe45233
MD. Zonaet Hossain Munna <zonaetmonna@gmail.com>
2026-02-03 10:12:08 +0600

    editor setup

--- Data_Structures/pointers/pointers.go
+++ Data_Structures/pointers/pointers.go
@@ -3,27 +3,27 @@ package main
 import "fmt"
 
 func main() {
-    // 1. Basic Variable
-    num := 100
+	// 1. Basic Variable
+	num := 100
 
-    // 2. Creating a Pointer (Store the address of num)
-    // '&' operator gets the address
-    
-    ptr := &num 
+	// 2. Creating a Pointer (Store the address of num)
+	// '&' operator gets the address
 
-    fmt.Println("--- TOPIC: POINTERS ---")
-    fmt.Println("Value of num:   ", num)      // 100
-    fmt.Println("Address of num: ", ptr)      // e.g., 0xc0000120f0
+	ptr := &num
 
-    // 3. Dereferencing (Access value via address)
-    // '*' operator gets value from the pointer
-    fmt.Println("Value via ptr:  ", *ptr)    // 100
+	fmt.Println("--- TOPIC: POINTERS ---")
+	fmt.Println("Value of num:   ", num) // 100
+	fmt.Println("Address of num: ", ptr) // e.g., 0xc0000120f0
 
-    // 4. Changing value through the pointer
-    *ptr = 200
-    fmt.Println("New Value of num:", num)   // num is now 200
+	// 3. Dereferencing (Access value via address)
+	// '*' operator gets value from the pointer
+	fmt.Println("Value via ptr:  ", *ptr) // 100
 
-    // 5. Pointer Zero Value
-    var emptyPtr *int
-    fmt.Println("Nil Pointer:    ", emptyPtr) // <nil>
+	// 4. Changing value through the pointer
+	*ptr = 200
+	fmt.Println("New Value of num:", num) // num is now 200
+
+	// 5. Pointer Zero Value
+	var emptyPtr *int
+	fmt.Println("Nil Pointer:    ", emptyPtr) // <nil>
 }
